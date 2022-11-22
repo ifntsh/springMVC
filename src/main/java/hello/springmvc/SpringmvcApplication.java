51ba74e5e52ef6dfe98232dc9d6cbcc38df10d21
ifntsh <bizifntsh@gmail.com>
2022-11-22 22:14:41 +0900

    Koyeb 테스트용 main에 getmapping추가

--- src/main/java/hello/springmvc/SpringmvcApplication.java
+++ src/main/java/hello/springmvc/SpringmvcApplication.java
@@ -2,10 +2,12 @@ package hello.springmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SpringmvcApplication {
 
+	@GetMapping("")
 	public static void main(String[] args) {
 		SpringApplication.run(SpringmvcApplication.class, args);
 	}
